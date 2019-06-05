f4ab70c854bef4b1c2d6a1e58c760178c324849b
hashdogg <hashdoggie@gmail.com>
2019-06-05 22:22:04 +0100

    removed sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './MAI-Youth-Logo.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Homepage">
+            <div className="Top-bar">
+                <img className="Logo" alt="MAI Logo" src={logo}/>
+                <h1 className="Title">MAI Youth</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
