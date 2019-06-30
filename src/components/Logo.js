6c6bdd37034089b6d7a7246939956ed7237f1c62
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-06-30 13:06:41 +0100

    Renamed assets folder

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
-import logo from '../Assets/maiLogo.jpg';
+import logo from '../assets/maiLogo.jpg';
 
 const Logo = <Image size="tiny" src={logo} circular />;
 
