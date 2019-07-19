519d154f1f7a240de452e10e29057a18b05f3b7f
salahusama <salaheldin.akl@student.dit.ie>
2019-07-19 23:29:05 +0100

    Apply design to page structure

--- src/components/homepage/Header.js
+++ src/components/homepage/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
 import Logo from '../Logo';
 
 export default function Header() {
   return (
-    <div className="Header">
-      <div className="HeaderLeft">
-        <Logo />
-        <h1 className="HeaderTitle">MAI Youth</h1>
-      </div>
-      <div className="HeaderRight">
-        <h2 className="HeaderHome">Home</h2>
-        <h2 className="HeaderArticles">Articles</h2>
-        <h2 className="HeaderContact">Contact Us</h2>
-      </div>
-    </div>
+    <Menu
+      borderless
+      fixed="top"
+      className="menu-header"
+    >
+      <Menu.Item><Logo /></Menu.Item>
+      <Menu.Item className="menu-item">MAI Youth</Menu.Item>
+
+      <Menu.Menu position="right">
+        <Menu.Item className="menu-item" href="/articles">Articles</Menu.Item>
+        <Menu.Item className="menu-item" href="/events">Upcoming Events</Menu.Item>
+        <Menu.Item className="menu-item" href="/contact">Contact Us</Menu.Item>
+      </Menu.Menu>
+    </Menu>
   );
 }
