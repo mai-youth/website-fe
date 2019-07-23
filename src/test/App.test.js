13a79c0b513c79c8626748a455a229c697c923f3
Salaheldin Akl <salaheldin.akl@gmail.com>
2019-07-23 02:13:04 +0200

    Add ability to add new articles from admin page (#31)

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from '../Router';
+import App from '../components/HomePage/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Router />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
