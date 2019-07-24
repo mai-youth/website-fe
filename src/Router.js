8a3060899971736837598af6b0950ce470d35786
salahusama <salaheldin.akl@student.dit.ie>
2019-07-25 00:50:52 +0100

    Add articles main page

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import App from './components/HomePage/App';
 import AdminLayout from './components/AdminPage/AdminLayout';
+import ArticlesPage from './components/articles/ArticlesPage';
 
 export default function Router() {
   return (
@@ -9,6 +10,8 @@ export default function Router() {
       <Switch>
         <Redirect exact from="/" to="/app" />
         <Route path="/app" component={App} />
+        <Route path="/articles/:id" component={({ match }) => `Sorry, under construction. You requested article#${match.params.id}.`} />
+        <Route path="/articles" component={ArticlesPage} />
         <Route path="/admin" component={AdminLayout} />
       </Switch>
     </BrowserRouter>
