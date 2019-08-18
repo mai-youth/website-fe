c690ba118fdc65965871769d0921e3907ecf2e3f
Salaheldin Akl <salaheldin.akl@gmail.com>
2019-08-18 04:48:08 +0100

    Add articles main page (#42)
    * Update circleci config to latest node version

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/HomePage/App';
+import ArticlesPage from './components/articles/ArticlesPage';
 import AdminPage from './components/AdminPage/AdminPage';
 import ContactPage from './components/ContactPage/ContactPage';
 
@@ -11,6 +12,8 @@ export default function Router() {
         <Route exact path="/" component={App} />
         <Route exact path="/admin" component={AdminPage} />
         <Route exact path="/contact" component={ContactPage} />
+        <Route path="/articles/:id" component={({ match }) => `Sorry, under construction. You requested article#${match.params.id}.`} />
+        <Route path="/articles" component={ArticlesPage} />
         <Route path="/" component={App} />
       </Switch>
     </BrowserRouter>
