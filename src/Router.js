a61c9ccb6837ce5e448e53fa5174e88846a858fe
Salaheldin Akl <salaheldin.akl@gmail.com>
2019-08-18 04:31:50 +0100

    28 edit articles (#39)
    * Rename main admin component
    
    * Make modal more modular + add edit article functionality

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/HomePage/App';
-import AdminLayout from './components/AdminPage/AdminLayout';
+import AdminPage from './components/AdminPage/AdminPage';
 import ContactPage from './components/ContactPage/ContactPage';
 
 export default function Router() {
@@ -9,7 +9,7 @@ export default function Router() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={App} />
-        <Route exact path="/admin" component={AdminLayout} />
+        <Route exact path="/admin" component={AdminPage} />
         <Route exact path="/contact" component={ContactPage} />
         <Route path="/" component={App} />
       </Switch>
--- src/components/AdminPage/ArticleTableRows.js
+++ src/components/AdminPage/ArticleTableRows.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import { Table, Button, Icon } from 'semantic-ui-react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { Table, Button } from 'semantic-ui-react';
+import NewArticleModal from './NewArticleModal';
+import { editArticle } from '../../actions/articles';
 
-export default function ArticleTableRows({ articles }) {
-  if (!articles || articles.length === 0) {
-    return null;
+class ArticleTableRows extends PureComponent {
+  render() {
+    // eslint-disable-next-line no-shadow
+    const { articles, editArticle } = this.props;
+
+    if (!articles || articles.length === 0) {
+      return null;
+    }
+
+    return articles.map(({ id, title, body, author }) => (
+      <Table.Row key={id}>
+        <Table.Cell>{title}</Table.Cell>
+        <Table.Cell>{author}</Table.Cell>
+        <Table.Cell>{`${body.slice(0, 25)}...`}</Table.Cell>
+        <Table.Cell>
+          <NewArticleModal
+            onSubmit={updated => editArticle({ id, title, body, author }, updated)}
+            modalTitle="Edit Article"
+            defaultValues={{ title, body, author }}
+            trigger={<Button size="mini" icon="pencil" />}
+          />
+          <Button color="red" size="mini" icon="delete" />
+        </Table.Cell>
+      </Table.Row>
+    ));
   }
-
-  return articles.map(({ id, title, body, author }) => (
-    <Table.Row key={id}>
-      <Table.Cell>{title}</Table.Cell>
-      <Table.Cell>{author}</Table.Cell>
-      <Table.Cell>{`${body.slice(0, 25)}...`}</Table.Cell>
-      <Table.Cell>
-        <Button size="mini" icon>
-          <Icon name="pencil" />
-        </Button>
-        <Button color="red" size="mini" icon>
-          <Icon name="delete" />
-        </Button>
-      </Table.Cell>
-    </Table.Row>
-  ));
 }
+
+ArticleTableRows.propTypes = {
+  articles: PropTypes.array.isRequired,
+  editArticle: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  editArticle: (old, updated) => dispatch(editArticle(old, updated)),
+});
+
+export default connect(null, mapDispatchToProps)(ArticleTableRows);
