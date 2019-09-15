dbeb394cc8292932bebd13f35f24609415a93234
salahusama <salaheldin.akl@student.dit.ie>
2019-09-15 08:12:46 +0100

    Rename folder name: contact

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/homepage/App';
 import ArticlesPage from './components/articles/ArticlesPage';
 import ArticleView from './components/articles/ArticleView';
-import AdminPage from './components/AdminPage/AdminPage';
+import AdminPage from './components/admin/AdminPage';
 import ContactPage from './components/ContactPage/ContactPage';
 
 export default function Router() {
